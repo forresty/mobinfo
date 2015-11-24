043365979a747730fe6830b54ae95c21cd176a54
Forrest Ye <afu@forresty.com>
2015-11-24 17:29:27 +0800

    lookup with binary search

--- lib/mobinfo.rb
+++ lib/mobinfo.rb
@@ -1,4 +1,56 @@
 require "mobinfo/version"
 
 module MobInfo
+  @@data = File.read(File.expand_path('../../data/phone.dat', __FILE__), mode: 'rb')
+  @@data_version = @@data[0..3]
+  @@first_index_offset = @@data[4..7].unpack('l').first
+
+  def self.record_at_offset(offset)
+    string = @@data[offset..-1].unpack('Z*').first.force_encoding('utf-8')
+
+    province, city, postal_code, region_code = string.split('|')
+
+    { province: province, city: city, postal_code: postal_code, region_code: region_code }
+  end
+
+  def self.binary_search(prefix, from=0, to=nil)
+    if to.nil?
+      to = ((@@data.size - @@first_index_offset) / 9).to_i
+    end
+
+    if from > to
+      return [prefix, nil, nil]
+    end
+
+    mid = (from + to) / 2
+
+    index_offset = mid * 9 + @@first_index_offset
+
+    first_7digits, record_offset, type = @@data[index_offset, index_offset+9].unpack('l2c')
+
+    if first_7digits.nil?
+      return [prefix, nil, nil]
+    end
+
+    if prefix < first_7digits
+      binary_search(prefix, from, mid - 1)
+    elsif prefix > first_7digits
+      binary_search(prefix, mid + 1, to)
+    else
+      [first_7digits, record_offset, type]
+    end
+  end
+
+  def self.lookup(number)
+    if number =~ /\+86-/
+      number = number[4..-1]
+    end
+    prefix = number[0...7].to_i
+
+    first_7digits, record_offset, type = binary_search(prefix)
+
+    if type
+      record_at_offset(record_offset)
+    end
+  end
 end
--- spec/mobinfo_spec.rb
+++ spec/mobinfo_spec.rb
@@ -4,4 +4,13 @@ describe MobInfo do
   it 'has a version number' do
     expect(MobInfo::VERSION).not_to be nil
   end
+
+  it 'finds number' do
+    expect(MobInfo.lookup('1300000')[:city]).to eq('济南')
+    expect(MobInfo.lookup('1862169')[:province]).to eq('上海')
+    expect(MobInfo.lookup('18621690000')[:province]).to eq('上海')
+    expect(MobInfo.lookup('+86-18621690000')[:province]).to eq('上海')
+    expect(MobInfo.lookup('+852-67470000')).to be_nil
+    expect(MobInfo.lookup('2000000')).to be_nil
+  end
 end
